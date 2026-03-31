8423ba000a8a7cee55ad50760a41c4598d7e10d3
Devendra Kumar Ojha <devojha0099@gmail.com>
2026-03-31 23:00:40 +0530

    frontend updated with backend url

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react(),tailwindcss()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5001',
+      '/api': 'https://your-app.onrender.com',
     },
   },
 })
